2a3a831e2a3b4b86ee2de126482ca92813db5284
Oğulcan <40337665+ogulcankarayel5@users.noreply.github.com>
2019-11-28 00:27:40 +0300

    Profile resmi guncelleme ve firebase storage eklendi

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterinstagramclone/models/user_model.dart';
 import 'package:flutterinstagramclone/services/database_service.dart';
+import 'package:flutterinstagramclone/services/storage_service.dart';
 import 'package:image_picker/image_picker.dart';
-
-
-
 
 class EditProfileScreen extends StatefulWidget {
   final User user;
@@ -22,20 +21,30 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   File _profileImage;
   String _name = '';
   String _bio = '';
+  bool _isLoading = false;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     _name = widget.user.name;
     _bio = widget.user.bio;
   }
 
- 
-  _submit() {
+  _submit() async {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
+      setState(() {
+        _isLoading = true;
+      });
       // Update user in database
       String _profileImageUrl = '';
+
+      if (_profileImage == null) {
+        _profileImageUrl = widget.user.profileImageUrl;
+      } else {
+        _profileImageUrl = await StorageService.uploadUserProfileImage(
+            widget.user.profileImageUrl, _profileImage);
+      }
       User user = User(
         id: widget.user.id,
         name: _name,
@@ -49,29 +58,32 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     }
   }
 
-  _handleImageFromGallery() async{
-    File imageFile=await ImagePicker.pickImage(source: ImageSource.gallery);
-    if(imageFile!=null){
+  _handleImageFromGallery() async {
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
       setState(() {
-        _profileImage=imageFile;
+        _profileImage = imageFile;
       });
     }
   }
 
-  _displayProfileImage(){
+  _displayProfileImage() {
     //No new profile image
-    if(_profileImage==null){
+    if (_profileImage == null) {
       //no existing profile image
-      if(widget.user.profileImageUrl.isEmpty){
+      if (widget.user.profileImageUrl.isEmpty) {
         //display placeholder
         return AssetImage('assets/images/indir.jpg');
+      } else {
+        //user profile image exists
+        return CachedNetworkImageProvider(widget.user.profileImageUrl);
       }
-    }
-    else{
+    } else {
       //new profile image
       return FileImage(_profileImage);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,77 +95,85 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 60.0,
-                    backgroundColor: Colors.grey,
-                    backgroundImage:
-                        _displayProfileImage(),
-                  ),
-                  FlatButton(
-                    onPressed: () =>_handleImageFromGallery(),
-                    child: Text(
-                      'Change Profile Image',
-                      style: TextStyle(
-                          color: Theme.of(context).accentColor, fontSize: 16.0),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: ListView(
+          children: <Widget>[
+            _isLoading
+                ? LinearProgressIndicator(
+                    backgroundColor: Colors.blue[200],
+                    valueColor: AlwaysStoppedAnimation(Colors.blue),
+                  )
+                : SizedBox.shrink(),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: _displayProfileImage(),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.person,
-                        size: 30.0,
-                      ),
-                      labelText: 'Name',
-                    ),
-                    validator: (input) => input.trim().length < 1
-                        ? 'Please enter a valid name'
-                        : null,
-                    onSaved: (input) => _name = input,
-                  ),
-                  TextFormField(
-                    initialValue: _bio,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.book,
-                        size: 30.0,
-                      ),
-                      labelText: 'Bio',
-                    ),
-                    validator: (input) => input.trim().length > 150
-                        ? 'Please enter a bio less than 150 characters'
-                        : null,
-                    onSaved: (input) => _bio = input,
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(40.0),
-                    height: 40.0,
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      textColor: Colors.white,
+                    FlatButton(
+                      onPressed: () => _handleImageFromGallery(),
                       child: Text(
-                        'Save Profile',
-                        style: TextStyle(fontSize: 18.0),
+                        'Change Profile Image',
+                        style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                            fontSize: 16.0),
                       ),
                     ),
-                  ),
-                ],
+                    TextFormField(
+                      initialValue: _name,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.person,
+                          size: 30.0,
+                        ),
+                        labelText: 'Name',
+                      ),
+                      validator: (input) => input.trim().length < 1
+                          ? 'Please enter a valid name'
+                          : null,
+                      onSaved: (input) => _name = input,
+                    ),
+                    TextFormField(
+                      initialValue: _bio,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.book,
+                          size: 30.0,
+                        ),
+                        labelText: 'Bio',
+                      ),
+                      validator: (input) => input.trim().length > 150
+                          ? 'Please enter a bio less than 150 characters'
+                          : null,
+                      onSaved: (input) => _bio = input,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(40.0),
+                      height: 40.0,
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text(
+                          'Save Profile',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
